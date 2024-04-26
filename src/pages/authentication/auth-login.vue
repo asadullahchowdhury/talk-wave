543fab34c815fd65cfd5438f56832c3e2d6b8fe0
Asadullah Chowdhury Rahat <asadullah_chowdhury@outlook.com>
2024-04-26 15:42:41 +0600

    [login UI completed]

--- src/pages/authentication/auth-login.vue
+++ src/pages/authentication/auth-login.vue
@@ -1,24 +1,25 @@
 <template>
   <div class="container-fluid">
     <div class="row">
-      <div class="col-lg-5 col-md-6 min-vh-100 bg-white">
+      <!--Left auth box start-->
+      <div class="col-xxl-4 col-lg-5 col-md-6 min-vh-100 position-relative left-auth-box">
         <!--Authentication form start-->
         <div class="p-md-5 p-4 h-100 d-flex  align-items-center w-100">
 
-          <form class=" w-100" autocomplete="off">
+          <form class="w-100" autocomplete="off">
 
             <!--Form Header start-->
             <div class="logo fw-bold fs-2 mb-3 ">
               <img src="https://themes.pixelstrap.com/chitchat/assets/images/logo/landing-logo.png" alt="">
             </div>
-            <h3 class="fw-semibold">Hello Everyone , We are Chitchat</h3>
-            <p class="text-muted mb-4">Wellcome to chitchat please login to your account.</p>
+            <h3 class="fw-semibold">Hello Everyone , We are Chatecho</h3>
+            <p class="text-muted mb-4">Welcome to Chatecho please login to your account.</p>
             <!--Form Header end  -->
 
             <!--Email input-->
             <div class="form-group mb-4">
               <label for="email" class="form-label fw-semibold">Email Address</label>
-              <input type="email" class="form-control w-100 shadow-none"
+              <input type="email" class="form-control rounded-pill ps-4 w-100 shadow-none"
                      placeholder="Enter your email address" name="email">
               <div class="error-report"></div>
             </div>
@@ -26,7 +27,7 @@
             <!--Password input-->
             <div class="form-group mb-4">
               <label for="password" class="form-label fw-semibold">Password</label>
-              <input type="password" class="form-control w-100 shadow-none"
+              <input type="password" class="form-control rounded-pill ps-4 w-100 shadow-none"
                      placeholder="Enter your password" name="password">
               <div class="error-report"></div>
             </div>
@@ -46,8 +47,8 @@
 
             <!--Action button start-->
             <div class="form-group my-5 d-flex gap-3 justify-content-center">
-              <button type="submit" class="btn btn-primary rounded-pill px-5 py-2">Login</button>
-              <a href="javascript:void(0)" class="btn btn-dark rounded-pill px-5 py-2">Signup</a>
+              <button type="submit" class="btn btn-primary rounded-pill width-150 height-50">Login</button>
+              <button type="butto5" class="btn btn-dark rounded-pill width-150 height-50">Signup</button>
             </div>
             <!--Action button end  -->
 
@@ -91,7 +92,10 @@
         </div>
         <!--Authentication form end  -->
       </div>
-      <div class="col-lg-7 col-md-6 d-md-block d-none min-vh-100 position-fixed end-0 right-side">
+      <!--Left auth box end  -->
+
+      <!--Right auth box start-->
+      <div class="col-xxl-8 col-lg-7 col-md-6 d-md-flex min-vh-100 position-fixed end-0 right-auth-box">
         <div class="bg-circle">
           <div></div>
           <div></div>
@@ -117,7 +121,19 @@
         <div class="bottom-circle1"></div>
         <div class="right-circle"></div>
         <div class="right-circle1"></div>
+        <img class="heart-logo" src="../../assets/image/auth/heart.png" alt="heart">
+        <img class="hash-logo" src="../../assets/image/auth/hash.png" alt="hash">
+        <img class="auth-img" src="../../assets/image/auth/login.png" alt="login">
+        <img class="boy-img" src="../../assets/image/auth/boy.png" alt="boy">
+        <img class="girl-img" src="../../assets/image/auth/girl.png" alt="girl">
+        <img class="cloud-logo" src="../../assets/image/auth/cloud.png" alt="cloud">
+        <img class="cloud-logo1" src="../../assets/image/auth/cloud.png" alt="cloud">
+        <img class="cloud-logo2" src="../../assets/image/auth/cloud.png" alt="cloud">
+        <img class="cloud-logo3" src="../../assets/image/auth/cloud.png" alt="cloud">
+        <img class="cloud-logo4" src="../../assets/image/auth/cloud.png" alt="cloud">
+        <img class="hash-logo1" src="../../assets/image/auth/cloud.png" alt="cloud">
       </div>
+      <!--Right auth box end  -->
     </div>
   </div>
 </template>

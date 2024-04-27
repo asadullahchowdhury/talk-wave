c38e9fea09c7f924bb578594f9a3ee41888a44b5
Asadullah Chowdhury Rahat <asadullah_chowdhury@outlook.com>
2024-04-27 19:30:09 +0600

    ui update

--- src/layout/components/main-header.vue
+++ src/layout/components/main-header.vue
@@ -1,27 +1,19 @@
 <template>
-  <div class="header">
+  <div class="header w-100">
     <nav class="navbar navbar-expand-lg">
       <div class="container">
-        <a class="navbar-brand" href="#"><b>Chat</b>Echo</a>
+        <a class="navbar-brand fs-3" href="#"><b>Chat</b>Echo</a>
 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-              <router-link :to="{name:'InboxDashboard'}" class="nav-link" href="#">Dashboard</router-link>
-            </li>
-          </ul>
-
-          <router-link :to="{name:'AuthLogin'}" class="btn btn-outline-dark rounded-pill px-4 mx-2">Login</router-link>
-          <router-link :to="{name:'AuthRegister'}" class="btn btn-dark rounded-pill px-4 mx-2">Signup</router-link>
+        <div class="collapse navbar-collapse " id="navbarSupportedContent">
+         <div class="ms-auto">
+           <router-link :to="{name:'AuthLogin'}" class="btn btn-outline-dark fw-semibold rounded-pill px-4 mx-2">Login</router-link>
+           <router-link :to="{name:'AuthRegister'}" class="btn btn-dark fw-semibold rounded-pill px-4 mx-2">Signup</router-link>
+         </div>
         </div>
       </div>
     </nav>
--- src/pages/home-page.vue
+++ src/pages/home-page.vue
@@ -1,6 +1,23 @@
 <template>
-  <div class="vh-100 d-flex justify-content-center align-items-center">
-<h1 class="display-1 ">Home</h1>
+  <div class="homepage">
+    <div class="container">
+      <div class="hero-section ">
+        <div class="row justify-content-center">
+          <div class="col-md-7 text-center">
+            <h1 class="hero-title"><span>One click.</span> Zero Friction</h1>
+
+            <p class="hero-sub">ChatEcho is an instant messaging tool that connects prospects directly to real
+              salespeople. That's it. No
+              bots.
+              Nothing to download. Just real, one-on-one connections for truly frictionless sales</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="text-center">
+      <img class="home-bg mx-auto img-fluid" src="../assets/image/bg-2.png" alt="">
+    </div>
   </div>
 </template>
 
